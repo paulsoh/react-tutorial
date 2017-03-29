9621446706b542b13981c0a256562253fb7e8704
Paul Yongseok Soh <yongdoree@gmail.com>
2017-03-29 15:04:43 +0900

    Add View for 단어장

--- src/App.js
+++ src/App.js
@@ -3,8 +3,165 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        멋진 단어장
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>단어장 튜토리얼</h1>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-lg-12">
+            <ul className="nav nav-tabs">
+              <li className="active">
+                <a href="#">
+                  모든 단어
+                  <span className="badge">12</span>
+                </a>
+              </li>
+              <li>
+                <a>
+                  학습완료한 단어
+                  <span className="badge">2</span>
+                </a>
+              </li>
+              <li>
+                <a>
+                  학습중인 단어
+                  <span className="badge">10</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Search for..." />
+              <span className="input-group-btn">
+                <button className="btn btn-default" type="button">단어 추가</button>
+              </span>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="list-group">
+              <a className="list-group-item">
+                prevaricate
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                hackneyed
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                diffident
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                plucky
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                meek
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                insidious
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                embellishment
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                forthcoming
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                subvert
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                esoteric
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+              <a className="list-group-item">
+                unassailable
+                <div className="pull-right">
+                  <div className="btn-group btn-group-xs" role="group" aria-label="...">
+                    <button type="button" className="btn btn-success">설명 보기</button>
+                    <button type="button" className="btn btn-primary">학습 완료</button>
+                    <button type="button" className="btn btn-warning">학습 필요</button>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
