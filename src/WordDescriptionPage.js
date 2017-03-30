a34e65cd44c061609b730c5bb59d536e56b88300
Paul Yongseok Soh <yongdoree@gmail.com>
2017-03-30 13:31:53 +0900

    Add style for WordDescriptionPage

--- src/WordDescriptionPage.js
+++ src/WordDescriptionPage.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 class WordDescriptionPage extends React.Component {
   render = () => {
+    const word = this.props.match.params.word;
+
     return (
-      <div>
-        Hello, 당신이 찾아보신 단어는 {`${this.props.match.params.word}`} 입니다! 
+      <div className="container">
+        <br />
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">{word}</h3>
+              </div>
+              <div className="panel-body">
+                <h5>
+                  <span className="label label-primary">
+                    단어 정의
+                  </span>
+                  &nbsp;단어 설명 주루룩
+                </h5>
+                <p>
+                  <span className="label label-success">
+                    단어 정의
+                  </span>
+                  &nbsp;예문 주루룩
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
